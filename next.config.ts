f9acaa98f2e0ab12403e9367bf68993b8f03132a
Muhammad <muhammad.safarov@web.de>
2025-03-31 20:03:07 +0200

    Done changes for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/portfolio',
+  images: {
+    unoptimized: true, // GitHub Pages doesn't support Next.js image optimization
+  },
 };
 
 export default nextConfig;
