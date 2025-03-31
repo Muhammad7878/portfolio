5ea1089335dc3562aca21f962991c78f03290c35
Muhammad <muhammad.safarov@web.de>
2025-03-31 21:24:57 +0200

    Solved document not defined issue

--- components/ui/GradientBG.tsx
+++ components/ui/GradientBG.tsx
@@ -39,23 +39,30 @@ export const BackgroundGradientAnimation = ({
   const [curY, setCurY] = useState(0);
   const [tgX, setTgX] = useState(0);
   const [tgY, setTgY] = useState(0);
+  const [isClient, setIsClient] = useState(true)
   useEffect(() => {
-    document.body.style.setProperty(
+    setIsClient(true); 
+  }, []);
+  useEffect(() => {
+    isClient && document.body.style.setProperty(
       "--gradient-background-start",
       gradientBackgroundStart
     );
-    document.body.style.setProperty(
+    isClient && document.body.style.setProperty(
       "--gradient-background-end",
       gradientBackgroundEnd
     );
-    document.body.style.setProperty("--first-color", firstColor);
-    document.body.style.setProperty("--second-color", secondColor);
-    document.body.style.setProperty("--third-color", thirdColor);
-    document.body.style.setProperty("--fourth-color", fourthColor);
-    document.body.style.setProperty("--fifth-color", fifthColor);
-    document.body.style.setProperty("--pointer-color", pointerColor);
-    document.body.style.setProperty("--size", size);
-    document.body.style.setProperty("--blending-value", blendingValue);
+    if(isClient){
+
+      document.body.style.setProperty("--first-color", firstColor);
+      document.body.style.setProperty("--second-color", secondColor);
+      document.body.style.setProperty("--third-color", thirdColor);
+      document.body.style.setProperty("--fourth-color", fourthColor);
+      document.body.style.setProperty("--fifth-color", fifthColor);
+      document.body.style.setProperty("--pointer-color", pointerColor);
+      document.body.style.setProperty("--size", size);
+      document.body.style.setProperty("--blending-value", blendingValue);
+    }
   }, []);
 
   useEffect(() => {
--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/portfolio',
-  images: {
-    unoptimized: true, // GitHub Pages doesn't support Next.js image optimization
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
